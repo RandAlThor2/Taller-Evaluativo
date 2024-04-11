5306b7a6776277b56fba1ec29b90715aeac65220
RandAlThor2 <montoya.fernandez202@gmail.com>
2024-04-11 14:31:28 -0500

    punto 1

--- tallercito.py
+++ tallercito.py
@@ -1,5 +1,12 @@
 import numpy as np
 import pandas as pt
 import matplotlib.pyplot as plt
+import scipy.io as sio
 
-prueba repositorio
+#1 aquí se creó una matriz que tuviera size 1,200,000 y se verificó
+
+array4D=np.random.rand(10,100,30,40)
+print(np.size(array4D))
+
+
+
