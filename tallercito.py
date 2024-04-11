94c2ed66a9f03a78ab222e7c24ee2680f042fed9
RandAlThor2 <montoya.fernandez202@gmail.com>
2024-04-11 14:37:49 -0500

    #2 Se crea la copia de la matriz, pero en 3 dimensiones eliminando la cuarta y se verifica el size tanto de la copia como del copiado

--- tallercito.py
+++ tallercito.py
@@ -8,5 +8,9 @@ import scipy.io as sio
 array4D=np.random.rand(10,100,30,40)
 print(np.size(array4D))
 
+#2 Se crea la copia de la matriz, pero en 3 dimensiones eliminando la cuarta y se verifica el size tanto de la copia como del copiado
 
+array3D=array4D.copy()[:,:,:,0]
+print(np.size(array3D))
+print(np.size(array4D))
 
